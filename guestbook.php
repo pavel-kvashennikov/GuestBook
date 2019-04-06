5e82ddfa6b5266f13356fd703d326bef558c7879
Pavel Kvashennikov <pavel.kvashennikov@outlook.com>
2019-04-06 15:30:59 +0500

    Mysql is worked

--- guestbook.php
+++ guestbook.php
@@ -1,26 +1,25 @@
-<?php
-require_once ('connect.php');
-$db=new db();
-$response = $db->query('SELECT * FROM users');
-//var_dump($response);
-?>
-
 <!doctype html>
 <html lang="ru">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/ajax.js"></script>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
     <title>Guest book</title>
 </head>
 <body>
-    
+    <form method="post" action="#">
+        <input type="text" name="name" id="name" placeholder="Введите имя">
+        <input type="text" name="message" id="message" placeholder="Введите текст">
+        <button id="btn">Отправить</button>
+        <button id="btn1">получить</button>
+    </form>
+    <div id="one"></div>
 </body>
 
-<script src="js/ajax.js"></script>
-<script src="js/jquery-3.2.1.slim.min.js"></script>
+
 <script src="js/popper.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 </body>
---
+++ js/ajax.js
@@ -0,0 +1,22 @@
+var $ = jQuery.noConflict();
+
+
+$(document).ready(function() {
+
+    $('#btn').click (function(){
+
+        $.ajax({
+            url:"post.php",
+            type:"POST",
+            cache:false,
+            data:{ name:$('#name').val(), message:$('#message').val() }, // Отправка
+            success: function (data) {
+                $('#one').html(data);
+            }
+
+        });
+
+    });
+
+
+});
